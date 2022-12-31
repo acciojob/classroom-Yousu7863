aef71a3543a4e9f23d2319f151f9dca8127c7a73
YOUSUF <mallick2598456@gmail.com>
2022-12-31 12:25:47 +0530

    initial

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -42,21 +42,18 @@ public class StudentController {
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
         Teacher teacher = studentService.getTeacherByNameService(name); // Assign student by calling service layer method
-
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
         List<String> students = studentService.getStudentsByTeacherNameService(teacher); // Assign list of student by calling service layer method
-
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
         List<String> students = studentService.getAllStudentService(); // Assign list of student by calling service layer method
-
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
